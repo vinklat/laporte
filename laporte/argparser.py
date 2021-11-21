5b038d05b2ae8aeb44319c94048896831cff8ad4
Václav Vinklát <vin@email.cz>
2021-11-21 19:54:04 +0100

    Refactor argparser
    (cherry picked from commit 8c72722b1bee7c63dd6fc247598a347840a06ec3)

--- laporte/argparser.py
+++ laporte/argparser.py
@@ -6,16 +6,23 @@ import os
 from argparse import ArgumentParser, ArgumentTypeError
 from .version import __version__, get_version_info
 
-_LOG_LEVEL_STRINGS = ['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG']
+LOG_LEVEL_STRINGS = ['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG']
+LOG_LEVEL_DEFAULT = 'DEBUG'
+LOG_VERBOSE_DEFAULT = False
+LISTEN_ADDR_DEFAULT = '0.0.0.0'
+LISTEN_PORT_DEFAULT = 9128
+CONFIG_DIR_DEFAULT = 'conf'
+CONFIG_FILE_DEFAULT = 'conf/sensors.yml'
+CONFIG_JINJA_DEFAULT = False
 
 
-def log_level_string_to_int(arg_string):
+def log_level_string_to_int(arg_string: str) -> int:
     '''get log level int from string'''
 
     log_level_string = arg_string.upper()
-    if log_level_string not in _LOG_LEVEL_STRINGS:
-        message = 'invalid choice: {0} (choose from {1})'.format(
-            log_level_string, _LOG_LEVEL_STRINGS)
+    if log_level_string not in LOG_LEVEL_STRINGS:
+        message = (f"invalid choice: {log_level_string} "
+                   f"(choose from {LOG_LEVEL_STRINGS})")
         raise ArgumentTypeError(message)
 
     log_level_int = getattr(logging, log_level_string, logging.INFO)
@@ -30,77 +37,76 @@ def get_pars():
 
     env_vars = {
         'LISTEN_ADDR': {
-            'default': '0.0.0.0'
+            'default': LISTEN_ADDR_DEFAULT
         },
         'LISTEN_PORT': {
-            'default': 9128
+            'default': LISTEN_PORT_DEFAULT
         },
         'CONFIG_FILE': {
-            'default': 'conf/sensors.yml'
+            'default': CONFIG_FILE_DEFAULT
         },
         'CONFIG_JINJA': {
-            'default': False
+            'default': CONFIG_JINJA_DEFAULT
         },
         'CONFIG_DIR': {
-            'default': 'conf'
+            'default': CONFIG_DIR_DEFAULT
         },
         'LOG_LEVEL': {
-            'default': 'DEBUG'
+            'default': LOG_LEVEL_DEFAULT
         },
         'LOG_VERBOSE': {
-            'default': False
+            'default': LOG_VERBOSE_DEFAULT
         },
     }
 
+    # defaults overriden from ENVs
     for env_var, env_pars in env_vars.items():
         if env_var in os.environ:
-            if isinstance(env_pars['default'], bool):
-                env_vars[env_var]['default'] = bool(os.environ[env_var])
-            elif isinstance(env_vars[env_var]['default'], int):
-                env_vars[env_var]['default'] = int(os.environ[env_var])
-            else:
-                env_vars[env_var]['default'] = os.environ[env_var]
-            env_vars[env_var]['required'] = False
+            default = os.environ[env_var]
+            if 'default' in env_pars:
+                if isinstance(env_pars['default'], bool):
+                    default = bool(os.environ[env_var])
+                elif isinstance(env_pars['default'], int):
+                    default = int(os.environ[env_var])
+            env_pars['default'] = default
+            env_pars['required'] = False
 
-    parser = ArgumentParser(description='Laporte {}'.format(__version__))
+    parser = ArgumentParser(description='Laporte {__version__}')
     parser.add_argument('-a',
                         '--listen-address',
                         action='store',
                         dest='listen_addr',
-                        help='listen address (default {0})'.format(
-                            env_vars['LISTEN_ADDR']['default']),
+                        help=f"listen address (default {LISTEN_ADDR_DEFAULT})",
                         type=str,
                         **env_vars['LISTEN_ADDR'])
     parser.add_argument('-p',
                         '--listen-port',
                         action='store',
                         dest='listen_port',
-                        help='listen port (default {0})'.format(
-                            env_vars['LISTEN_PORT']['default']),
+                        help=f"listen port (default {LISTEN_PORT_DEFAULT})",
                         type=int,
                         **env_vars['LISTEN_PORT'])
     parser.add_argument('-c',
                         '--config-file',
                         action='store',
                         dest='config_file',
-                        help='yaml or yaml+jinja2 file with '
-                        'sensor configuration (default {0})'.format(
-                            env_vars['CONFIG_FILE']['default']),
+                        help=("yaml or yaml+jinja2 file with "
+                              f"sensor configuration (default {CONFIG_DIR_DEFAULT})"),
                         type=str,
                         **env_vars['CONFIG_FILE'])
     parser.add_argument('-d',
                         '--config-dir',
                         action='store',
                         dest='config_dir',
-                        help='config directory (default {0})'.format(
-                            env_vars['CONFIG_DIR']['default']),
+                        help=f"config directory (default {CONFIG_DIR_DEFAULT})",
                         type=str,
                         **env_vars['CONFIG_DIR'])
     parser.add_argument('-j',
                         '--config-jinja',
                         action='store_true',
                         dest='config_jinja',
-                        help='use jinja2 in yaml config file',
+                        help=("use jinja2 in yaml config file "
+                              f"(default {CONFIG_JINJA_DEFAULT}"),
                         **env_vars['CONFIG_JINJA'])
     parser.add_argument('-V',
                         '--version',
@@ -110,9 +116,8 @@ def get_pars():
                         '--log-level',
                         action='store',
                         dest='log_level',
-                        help='set the logging output level. '
-                        '{0} (default {1})'.format(_LOG_LEVEL_STRINGS,
-                                                   env_vars['LOG_LEVEL']['default']),
+                        help=("set the logging output levelx "
+                              f"{LOG_LEVEL_STRINGS} (default {LOG_LEVEL_DEFAULT})"),
                         type=log_level_string_to_int,
                         **env_vars['LOG_LEVEL'])
     parser.add_argument('-v',
