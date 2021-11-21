42fdd6caa24e2c4b22fc028e29050a742cc4c23a
Václav Vinklát <vin@email.cz>
2021-11-21 19:55:20 +0100

    Refactor prometheus custom collector
    (cherry picked from commit 02ab6c1651507550f721403e3a2588b162709d1d)

--- laporte/metrics/collector.py
+++ laporte/metrics/collector.py
@@ -28,6 +28,42 @@ class CustomCollector():
         self.metrics = inner_metrics
         self.sensors = inner_sensors
 
+    def __get_label_keys(self, values_data: dict) -> list:
+        '''
+        get keys (names) of all labels
+        '''
+
+        if 'labels' not in values_data:
+            return []
+
+        labels = values_data['labels']
+        return list(map(itemgetter(0), labels.items()))
+
+    def __get_label_values(self, values_data: dict) -> list:
+        '''
+        get values of all labels
+        '''
+
+        if 'labels' not in values_data:
+            return []
+
+        labels = values_data['labels']
+        return list(map(itemgetter(1), labels.items()))
+
+    def __get_help_str(self, values_data: dict) -> str:
+        '''
+        generate string of help message
+        '''
+
+        label_keys = self.__get_label_keys(values_data)
+        if label_keys:
+            labels_str = ', '.join(label_keys)
+            help_str = f"{values_data['help_str']} (labels: {labels_str})"
+        else:
+            help_str = values_data['help_str']
+
+        return help_str
+
     def collect(self):
 
         # version info
@@ -43,43 +79,34 @@ class CustomCollector():
             for _, values_data in labels_data.items():  # unused values_key
                 count = values_data['count']
                 summ = values_data['sum']
-                labels = values_data['labels']
 
                 if metric_id not in families:
-                    label_keys = list(map(itemgetter(0), labels.items()))
-                    help_str = '{} (labels: {})'.format(
-                        values_data['help_str'],
-                        ', '.join(label_keys)) if label_keys else values_data['help_str']
                     met = SummaryMetricFamily(metric_id.split('[')[0],
-                                              help_str,
-                                              labels=label_keys)
+                                              self.__get_help_str(values_data),
+                                              labels=self.__get_label_keys(values_data))
                     families[metric_id] = met
                 else:
                     met = families[metric_id]
 
-                label_values = list(map(itemgetter(1), labels.items()))
-                met.add_metric(label_values, count, summ)
+                met.add_metric(self.__get_label_values(values_data), count, summ)
 
         # dump stored counters
         for metric_id, labels_data in self.metrics.counters.items():
             for _, values_data in labels_data.items():  # unused values_key
                 total = values_data['total']
-                labels = values_data['labels']
 
                 if metric_id not in families:
-                    label_keys = list(map(itemgetter(0), labels.items()))
-                    help_str = '{} (labels: {})'.format(
-                        values_data['help_str'],
-                        ', '.join(label_keys)) if label_keys else values_data['help_str']
                     met = CounterMetricFamily(metric_id.split('[')[0],
-                                              help_str,
-                                              labels=label_keys)
+                                              self.__get_help_str(values_data),
+                                              labels=self.__get_label_keys(values_data))
                     families[metric_id] = met
                 else:
                     met = families[metric_id]
 
-                label_values = list(map(itemgetter(1), labels.items()))
-                met.add_metric(label_values, total)
+                met.add_metric(self.__get_label_values(values_data), total)
+
+        for family in sorted(families, key=str.lower):
+            yield families[family]
 
         # dump laporte sensors
         for sensor in self.sensors.sensor_index:
@@ -95,7 +122,7 @@ class CustomCollector():
 
                 uniqname = f'{metric_name}_' + '_'.join(labels)
                 if uniqname not in families:
-                    help_str = 'with labels: {}'.format(', '.join(labels))
+                    help_str = f"with labels: {labels}"
                     if metric_type == COUNTER:
                         x = CounterMetricFamily(metric_name, help_str, labels=labels)
                     else:
